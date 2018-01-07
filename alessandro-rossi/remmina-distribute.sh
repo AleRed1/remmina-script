9a37797b27420738484aac95306d59c4f1ca1915
AleRed1 <alessandro_rossi2014@libero.it>
2018-01-07 20:57:24 +0100

    Finito

--- alessandro-rossi/remmina-distribute.sh
+++ alessandro-rossi/remmina-distribute.sh
@@ -9,6 +9,7 @@ for user in $utenti; do
 
         sed "s/10.2.60.100/$IP/g" VM.remmina > VM-$IP.remmina
         cp VM-$IP.remmina /home/$user/Scrivania/
-        sed "s/VM.remmina/VM-$IP.remmina/g" remoto.sh > /home/$user/Scrivania/remoto-$IP.sh
+        sed "s/VM.remmina/VM-$IP.remmina/g" remoto.sh > /home/$user/Desktop/ProvaRemoto/remoto-$IP.sh
+    #cambiato percorso
     done
 done
